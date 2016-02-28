8abc82a4ce53f2d01237a0c151e21ea1ef297d6f
jeremy <jeremy.a.heaton@gmail.com>
2016-02-28 09:31:05 -0500

    because

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 var app = require('express')();
-var http = require('https').Server(app);
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var redis = require('redis').createClient;
 var adapter = require('socket.io-redis');
