11abc38d9ae5bb357ddcfafb69b9dded72c00f00
Jeremy Heaton <jeremy.a.heaton@gmail.com>
2018-07-25 18:30:12 -0400

    update redis

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var redis = require('redis').createClient;
 var adapter = require('socket.io-redis');
-var pub = redis(7779, "ec2-54-225-248-13.compute-1.amazonaws.com", { auth_pass: "pcnht69peu3a041uv99dqd4qdti" });
-var sub = redis(7779, "ec2-54-225-248-13.compute-1.amazonaws.com", { return_buffers: true, auth_pass: "pcnht69peu3a041uv99dqd4qdti" });
-io.adapter(adapter({ pubClient: pub, subClient: sub }));
+var pub = redis(process.env.HEROKU_REDIS_CRIMSON_URL);
+var sub = redis(process.env.HEROKU_REDIS_CRIMSON_URL);
+io.adapter(adapter({pubClient: pub, subClient: sub}));
 var express = require('express');
 var request = require('request'); // "Request" library
 var querystring = require('querystring');
@@ -18,74 +18,80 @@ var authCode = null;																					// redirect
 // var redirect_uri = 'https://ancient-tor-6266.herokuapp.com/callback'; // Your
 // redirect uri
 
-app.get('/client/:room', function(req, res){
-	res.sendFile('public/client.html', {
-		root : __dirname
-	})
-	});
+app.get('/client/:room', function (req, res) {
+    res.sendFile('public/client.html', {
+        root: __dirname
+    })
+});
 
 
-io.sockets.on('connection', function(socket){
-
-    socket.on('subscribe', function(room) {
+io.sockets.on('connection', function (socket) {
+    socket.on('subscribe', function (room) {
         socket.join(room);
         var authOptions = {
-        		url : 'https://accounts.spotify.com/api/token',
-        		headers : {
-        			'Authorization' : 'Basic ' + 'ZDNiZmIzNmQ3NDRjNDkxZGI3NTdjMjgxOWRhYzczZWI6ZjI3ZjFhNGE1NTQwNGJlOTllNmJlYjE1M2M1NDI3OGI='
-        		},
-        		form : {
-                	grant_type: 'client_credentials'
-                },
-                json : true
-        	};
-        if(authCode == null){
-            request.post(authOptions, function(error, response, body) {
-        	    authCode = body.access_token;
-        	});
+            url: 'https://accounts.spotify.com/api/token',
+            headers: {
+                'Authorization': 'Basic ' + 'ZDNiZmIzNmQ3NDRjNDkxZGI3NTdjMjgxOWRhYzczZWI6ZjI3ZjFhNGE1NTQwNGJlOTllNmJlYjE1M2M1NDI3OGI='
+            },
+            form: {
+                grant_type: 'client_credentials'
+            },
+            json: true
+        };
+        if (authCode == null) {
+            request.post(authOptions, function (error, response, body) {
+                authCode = body.access_token;
+            });
         }
         io.sockets.in(room).emit('token', authCode);
+        console.log(room + ': connected')
         io.sockets.in(room).emit('fetchplaylist');
     });
 
-    socket.on('addSong', function(data){
-        io.sockets.in(data.room).emit('message', { room: data.room, message: data.message, song: data.song, artist: data.artist });
-     });
+    socket.on('addSong', function (data) {
+        console.log("attempting to add song");
+        io.sockets.in(data.room).emit('message',
+            {room: data.room, message: data.message, song: data.song, artist: data.artist});
+    });
 
-    socket.on('sendplaylist', function(data){
-  	 io.sockets.in(data.room).emit('playlist', data);
+    socket.on('songAdded', function (data) {
+        console.log(data);
+    });
+
+    socket.on('sendplaylist', function (data) {
+        io.sockets.in(data.room).emit('playlist', data);
     });
 });
 
 
-app.get('/refresh_token', function(req, res) {
+app.get('/refresh_token', function (req, res) {
 
-	// requesting access token from refresh token
-	var refresh_token = req.query.refresh_token;
-	var authOptions = {
-		url : 'https://accounts.spotify.com/api/token',
-		headers : {
-			'Authorization' : 'Basic '
-					+ (new Buffer(client_id + ':' + client_secret)
-							.toString('base64'))
-		},
-		form : {
-			grant_type : 'refresh_token',
-			refresh_token : refresh_token
-		},
-		json : true
-	};
+    // requesting access token from refresh token
+    var refresh_token = req.query.refresh_token;
+    var authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        headers: {
+            'Authorization': 'Basic '
+            + (new Buffer(client_id + ':' + client_secret)
+                .toString('base64'))
+        },
+        form: {
+            grant_type: 'refresh_token',
+            refresh_token: refresh_token
+        },
+        json: true
+    };
 
-	request.post(authOptions, function(error, response, body) {
-		if (!error && response.statusCode === 200) {
-			var access_token = body.access_token;
-			res.send({
-				'access_token' : access_token
-			});
-		}
-	});
+    request.post(authOptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var access_token = body.access_token;
+            res.send({
+                'access_token': access_token
+            });
+        }
+    });
 });
 
-http.listen(app.get('port'), function() {
-	console.log('Node app is running on port', app.get('port'));
+http.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
