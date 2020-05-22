f6bfd42b779bfe591bebfcedac358a403ff21c4f
Jeremy Heaton <jeremy.a.heaton@gmail.com>
2020-05-21 23:52:12 -0400

    coment code for now

--- index.js
+++ index.js
@@ -15,19 +15,15 @@ var request = require('request'); // "Request" library
 var querystring = require('querystring');
 app.set('port', (process.env.PORT || 8888));
 app.use(express.static('public'));
-var client_id = 'd3bfb36d744c491db757c2819dac73eb'; // Your client id
-var client_secret = 'f27f1a4a55404be99e6beb153c54278b'; // Your client secret
-var redirect_uri = (process.env.REDIRECT_URI || 'http://localhost:8888/callback'); // Your
-var authCode = null;																					// redirect
-// var redirect_uri = 'https://ancient-tor-6266.herokuapp.com/callback'; // Your
-// redirect uri
+// var client_id = 'd3bfb36d744c491db757c2819dac73eb'; // Your client id
+// var client_secret = 'f27f1a4a55404be99e6beb153c54278b'; // Your client secret
 
-app.get('/client/:room', function (req, res) {
-    res.sendFile('public/client.html', {
-        root: __dirname
-    })
-});
+// app.use(express.static(path.join(__dirname, 'client/build')));
 
+// // Handles any requests that don't match the ones above
+// app.get('/client/:room', (req,res) =>{
+//     res.sendFile(path.join(__dirname+'/client/build/index.html'));
+// });
 
 io.sockets.on('connection', function (socket) {
     socket.on('subscribe', function (room) {
@@ -57,35 +53,6 @@ io.sockets.on('connection', function (socket) {
 
     socket.on('sendplaylist', function (data) {
         io.sockets.in(data.room).emit('playlist', data);
-    });
-});
-
-
-app.get('/refresh_token', function (req, res) {
-
-    // requesting access token from refresh token
-    var refresh_token = req.query.refresh_token;
-    var authOptions = {
-        url: 'https://accounts.spotify.com/api/token',
-        headers: {
-            'Authorization': 'Basic '
-            + (new Buffer(client_id + ':' + client_secret)
-                .toString('base64'))
-        },
-        form: {
-            grant_type: 'refresh_token',
-            refresh_token: refresh_token
-        },
-        json: true
-    };
-
-    request.post(authOptions, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            var access_token = body.access_token;
-            res.send({
-                'access_token': access_token
-            });
-        }
     });
 });
 
